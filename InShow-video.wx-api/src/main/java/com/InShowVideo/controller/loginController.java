96d627389b0d35032ee8861c0e28e9460fa977e4
NayA000 <809104751@qq.com>
2018-09-19 20:09:15 +0800

    添加了在comment.utils中添加了https访问工具以及ssl自定义信任管理器

---
+++ InShow-video.wx-api/src/main/java/com/InShowVideo/controller/loginController.java
@@ -0,0 +1,34 @@
+package com.InShowVideo.controller;
+
+import com.InShowVideo.utils.JSONResult;
+
+public class loginController extends BasicController{
+	
+	public JSONResult login(
+			String js_code,
+			String encryptedData,
+			String iv,
+			String nickName,
+			String avatarUrl,
+			String gender,
+			String city,
+			String province,
+			String country) {
+		
+		//
+		String appID;
+		String appSecret;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		return JSONResult.ok();
+	}
+
+}
