088bf482e56ca820ef6a68217b89c1ef04c56cdc
AirYuBaby <1152519350@qq.com>
2018-09-29 08:23:31 +0800

    获取所有bgm列表前后端联调成功

--- InShow-video.wx-api/src/main/java/com/InShowVideo/WebMvcConfig.java
+++ InShow-video.wx-api/src/main/java/com/InShowVideo/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/**")
 		.addResourceLocations("classpath:/META-INF/resources/") 
-		.addResourceLocations("file:E:/fun-video/");
+		.addResourceLocations("file:E:/inShow-Video/");
 		
 	}
 	
--- InShow-video.wx-api/src/main/java/com/InShowVideo/controller/bgmController.java
+++ InShow-video.wx-api/src/main/java/com/InShowVideo/controller/bgmController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,38 +16,41 @@ import com.InShowVideo.utils.JSONResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+
 @RestController
-@Api(value="bgm相关的Controller",tags= {"bgm相关业务的controller"})
+@Api(value = "bgm相关的Controller", tags = { "bgm相关业务的controller" })
 @RequestMapping("/bgm")
 public class bgmController extends BasicController {
 	@Autowired
 	private bgmService bservice;
-	@ApiOperation(value="获取bgm列表的接口",notes="获取bgm列表的接口")
-	@ApiImplicitParam(name="page",value="动态加载的数据页数（从0开始）",required=true,dataType="int",paramType="query")
-	@GetMapping("/getAll")
-	public JSONResult getBgmByPage(int page) {
-		if(page>=0) {
-			List<Bgm> list = bservice.getAllbgm(page);
-			return JSONResult.ok(list);
-		}else {
+
+	@ApiOperation(value = "获取bgm列表的接口", notes = "获取bgm列表的接口")
+	@ApiImplicitParam(name = "page", value = "动态加载的数据页数（从0开始）", required = true, dataType = "int", paramType = "query")
+	@PostMapping("/getAll")
+	public JSONResult getBgmByPage(String page) {
+		if (page == null) {
 			return JSONResult.errorMsg("页码错误，你的小可爱呢不到视频啦");
+		} else {
+			List<Bgm> list = bservice.getAllbgm(0);
+			return JSONResult.ok(list);
 		}
+
 	}
-	
-	@ApiOperation(value="按热度获取bgm列表的接口",notes="按热度获取bgm列表的接口")
-	@ApiImplicitParam(name="page",value="动态加载的数据页数（从0开始）",required=true,dataType="int",paramType="query")
+
+	@ApiOperation(value = "按热度获取bgm列表的接口", notes = "按热度获取bgm列表的接口")
+	@ApiImplicitParam(name = "page", value = "动态加载的数据页数（从0开始）", required = true, dataType = "int", paramType = "query")
 	@GetMapping("/getAllByHart")
 	public JSONResult getBgmByHart(int page) {
-		//测试choose接口
-		//bservice.bgmBeChoose("1001");
-		
-		if(page>=0) {
+		// 测试choose接口
+		// bservice.bgmBeChoose("1001");
+
+		if (page >= 0) {
 			List<Bgm> list = bservice.getBgmByChoose(page);
-			for(Bgm bgm : list) {
+			for (Bgm bgm : list) {
 				System.out.println();
 			}
 			return JSONResult.ok(list);
-		}else {
+		} else {
 			return JSONResult.errorMsg("页码错误，你的小可爱呢不到视频啦");
 		}
 	}
